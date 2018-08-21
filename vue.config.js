a9ffc4168d6fe0a826f07567ac74627631c0e808
dengpeng <dp@tiejin.cn>
2018-08-21 14:29:17 +0800

    Fix

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,19 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     port: 8080,
-    disableHostCheck: true,
-    // productionSourceMap: false,
-    public: '10.3.0.27',
-    proxy: {
-      // 请求时匹配到command 
-      '/command/': {
-        target: 'http://api-sandbox.tiejin.cn/command/',
-        changeOrigin: true,
-        secure: false
-      },
-      '/file/upload/public': {
-        target: 'http://file-sandbox.tiejin.cn',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    // disableHostCheck: true,
+    // proxy: {
+    //   // 请求时匹配到command 
+    //   '/command/': {
+    //     target: 'http://api-sandbox.tiejin.cn/command/',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/file/upload/public': {
+    //     target: 'http://file-sandbox.tiejin.cn',
+    //     changeOrigin: true,
+    //     secure: false
+    //   }
+    // }
   }
 }
