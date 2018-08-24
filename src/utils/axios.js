1c3ea338c90d2169743abd70634c7cf577224e6b
dengpeng <dp@tiejin.cn>
2018-08-24 16:25:03 +0800

    Fix  dev

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import api from '../utils/api';
 import Store from '../store'
 const axio = axios.create({ 
-	baseURL: process.env.BASE_API, // node环境的不同，对应不同的baseURL
+	// baseURL: process.env.BASE_API, // node环境的不同，对应不同的baseURL
 	timeout: 15000, // 请求的超时时间
 	  //设置默认请求头，使post请求发送的是formdata格式数据// axios的header默认的Content-Type好像是'application/json;charset=UTF-8',我的项目都是用json格式传输，如果需要更改的话，可以用这种方式修改
 	  // headers: { 
