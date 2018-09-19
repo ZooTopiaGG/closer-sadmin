bfde29995aba00f4c533c448e38b72c50907350c
邓鹏 <1109142362@qq.com>
2018-09-19 15:07:02 +0800

    rebuild

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,12 +1,11 @@
 <template>
   <section id="permission" class="flex flex-v">
-
     <section class="welcome">
-      <section>尊敬的 <span v-if="authUser.type === 1" class="name">超级管理员</span> <span class="name" v-else>普通管理员</span></section>
-      <section>欢迎来到超管后台!</section>
-      <section v-if="authUser.type != 1">在您拥有的权限范围内，</section>
-      <section v-else>超级无敌大Boss</section>
-      <section>请开始您的表演</section>
+      <!-- <section>尊敬的 <span v-if="authUser.type === 1" class="name">超级管理员</span> <span class="name" v-else>普通管理员</span></section> -->
+      <section>hello，{{ authUser.username }}！欢迎来到超管后台</section>
+      <!-- <section v-if="authUser.type != 1">在您拥有的权限范围内，</section> -->
+      <!-- <section v-else>超级无敌大Boss</section> -->
+      <!-- <section>请开始您的表演</section> -->
     </section>
   </section>
 </template>
